dce21b3ac228b74d92d8238eb8a883b9644df46c
IF2290 <246528729+IF2290@users.noreply.github.com>
2026-05-24 17:23:06 +0530

    Add round and score display (with js)

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 const roundResult = document.querySelector("#round-result");
 const result = document.querySelector("#result");
+const roundNo = document.querySelector("#round-no");
+const humanScoreDisplay = document.querySelector("#human-score");
+const computerScoreDisplay = document.querySelector("#computer-score");
+
 //CREATE FUNCTION gethumanChoice
     //CREATE the variable userInput and give it a value of a prompt asking it to write one of the three options
     //RETURN the result
@@ -24,39 +28,57 @@ function playGame(){
   let humanScore = 0;
   let computerScore = 0;
   let rounds = 0; 
+  computerScoreDisplay.textContent = "Computer score is: 0";
+  humanScoreDisplay.textContent = "Your score is: 0";
   function playRound(humanChoice, computerChoice){
     if (humanChoice === "rock" && computerChoice === "paper"){
     roundResult.textContent = "You lose! Paper beats rock!";
     computerScore++;
     rounds++;
+    computerScoreDisplay.textContent = "Computer score is: " + computerScore;
+    roundNo.textContent = "Round: " + rounds;
   } else if (humanChoice === "paper" && computerChoice === "rock"){
     roundResult.textContent = "you win! Paper beats rock!";
     humanScore++;
     rounds++;
+    humanScoreDisplay.textContent = "Your score is: " + humanScore;
+    roundNo.textContent = "Round: " + rounds;
   } else if (humanChoice === "scissors" && computerChoice === "rock"){
     roundResult.textContent = "you lose! Rock beats scissors!";
     computerScore++;
     rounds++;
+    computerScoreDisplay.textContent = "Computer score is: " + computerScore;
+    roundNo.textContent = "Round: " + rounds;
   } else if (humanChoice === "rock" && computerChoice === "scissors"){
     roundResult.textContent = "you win! Rock beats scissors!";
     humanScore++;
     rounds++;
+    humanScoreDisplay.textContent = "Your score is: " + humanScore;
+    roundNo.textContent = "Round: " + rounds;
   } else if (humanChoice === "paper" && computerChoice === "scissors"){
     roundResult.textContent = "you lose! Scissors beats paper!";
     computerScore++;
     rounds++;
+    computerScoreDisplay.textContent = "Computer score is: " + computerScore;
+    roundNo.textContent = "Round: " + rounds;
   } else if (humanChoice === "scissors" && computerChoice === "paper"){
     roundResult.textContent = "you win! Scissors beats paper!";
     humanScore++;
     rounds++;
+    humanScoreDisplay.textContent = "Your score is: " + humanScore;
+    roundNo.textContent = "Round: " + rounds;
   } else if (humanChoice === computerChoice){
     roundResult.textContent = "It's a draw!";
+    rounds++;
+    roundNo.textContent = "Round: " + rounds;
   }
   if (humanScore > computerScore && rounds === 5){
     result.textContent = "you win!!!";
   } else if (humanScore < computerScore && rounds === 5){
     result.textContent = "you lose :(";
-  };
+  } else if (humanScore === computerScore && rounds === 5){
+    result.textContent = "It's a draw!";
+  }
 }
  rock.addEventListener("click", (e) => {
     function getHumanChoice () {
